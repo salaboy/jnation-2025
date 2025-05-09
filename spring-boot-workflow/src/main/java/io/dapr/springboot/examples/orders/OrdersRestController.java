b2c584414cafd7eb9085b304c3d287d37c076641
salaboy <Salaboy@gmail.com>
2025-05-09 14:42:17 +0100

    adding workflow

---
+++ spring-boot-workflow/src/main/java/io/dapr/springboot/examples/orders/OrdersRestController.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2025 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.dapr.springboot.examples.orders;
+
+import io.dapr.spring.workflows.config.EnableDaprWorkflows;
+import io.dapr.springboot.examples.orders.workflow.OrderProcessingWorkflow;
+import io.dapr.workflows.client.DaprWorkflowClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@EnableDaprWorkflows
+public class OrdersRestController {
+
+  private final Logger logger = LoggerFactory.getLogger(OrdersRestController.class);
+
+  @Autowired
+  private DaprWorkflowClient daprWorkflowClient;
+
+  @Autowired
+  private OrdersStore orderStore;
+
+  @GetMapping("/")
+  public String root() {
+    return "OK";
+  }
+
+  private Map<String, String> ordersWorkflows = new HashMap<>();
+
+  /**
+   * Track customer endpoint.
+   *
+   * @param order provided customer to track
+   * @return confirmation that the workflow instance was created for a given customer
+   */
+  @PostMapping("/orders")
+  public String placeOrder(@RequestBody Order order) {
+    String instanceId = daprWorkflowClient.scheduleNewWorkflow(OrderProcessingWorkflow.class, order);
+    logger.info("Workflow instance " + instanceId + " started");
+    ordersWorkflows.put(order.getId(), instanceId);
+    return "New Workflow Instance created for Order: " + order.getId();
+  }
+
+  /**
+   *  Request customer follow-up.
+   *  @param order associated with a workflow instance
+   *  @return confirmation that the follow-up was requested
+   */
+  @PostMapping("/order/approve")
+  public String orderApproval(@RequestBody Order order) {
+    logger.info("Order approval requested: " + order.getId());
+    String workflowIdForCustomer = ordersWorkflows.get(order.getId());
+    if (workflowIdForCustomer == null || workflowIdForCustomer.isEmpty()) {
+      return "There is no workflow associated with customer: " + order.getId();
+    } else {
+      daprWorkflowClient.raiseEvent(workflowIdForCustomer, "OrderApprovalRequest", order);
+      return "Order Approval requested";
+    }
+  }
+
+
+  public Collection<Order> getOrders() {
+    return orderStore.getOrders();
+  }
+
+
+}
+
---
+++ spring-boot-workflow/src/main/java/io/dapr/springboot/examples/orders/workflow/OrderProcessingWorkflow.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2025 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.dapr.springboot.examples.orders.workflow;
+
+import io.dapr.springboot.examples.orders.Order;
+import io.dapr.workflows.Workflow;
+import io.dapr.workflows.WorkflowStub;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+public class OrderProcessingWorkflow implements Workflow {
+
+  @Override
+  public WorkflowStub create() {
+    return ctx -> {
+      String instanceId = ctx.getInstanceId();
+      Order order = ctx.getInput(Order.class);
+      order.setWorkflowId(instanceId);
+      ctx.getLogger().info("Let's store the order for tracking: " + order.getItem());
+      ctx.callActivity(TrackOrderActivity.class.getName(), order, Order.class).await();
+      ctx.getLogger().info("Let's check the if there is stock in the warehouse: " + order.getItem() + ".");
+      ctx.callActivity(CheckItemsStockActivity.class.getName(), order, Order.class).await();
+      ctx.getLogger().info("Let's wait for the customer to approve the order: " + order.getId());
+      order = ctx.waitForExternalEvent("Order Approved", Duration.ofMinutes(5), Order.class).await();
+
+      order = ctx.callActivity(ShipOrderActivity.class.getName(), order, Order.class).await();
+      ctx.getLogger().info("Congratulations your order is on the way: " + order.getItem() + "!");
+      ctx.complete(order);
+    };
+  }
+}
